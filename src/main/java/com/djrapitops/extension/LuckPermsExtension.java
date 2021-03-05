9632e7dab43a4b2d3200d0ad9ddc4dbce55282eb
Risto Lahtela <24460436+AuroraLS3@users.noreply.github.com>
2021-03-05 13:41:01 +0200

    Changed repository

--- src/main/java/com/djrapitops/extension/LuckPermsExtension.java
+++ src/main/java/com/djrapitops/extension/LuckPermsExtension.java
@@ -1,5 +1,5 @@
 /*
-    Copyright(c) 2019 Risto Lahtela (Rsl1122)
+    Copyright(c) 2019 Risto Lahtela (AuroraLS3)
 
     The MIT License(MIT)
 
@@ -45,7 +45,7 @@ import java.util.stream.Stream;
  * <p>
  * Adapted from PluginData by Vankka
  *
- * @author Rsl1122
+ * @author AuroraLS3
  */
 @PluginInfo(name = "LuckPerms", iconName = "exclamation-triangle", iconFamily = Family.SOLID, color = Color.LIGHT_GREEN)
 @TabInfo(tab = "Permission Groups", iconName = "users-cog", elementOrder = {})
